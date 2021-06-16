9ac8e5c6955efb63515276a20b8f5efc25c88b79
Tim <timothy.mcc@comcast.net>
2021-06-16 15:28:21 -0400

    added basic header

--- main.py
+++ main.py
@@ -12,40 +12,50 @@ WIDTH, HEIGHT = letter
 #     c.showPage()  # ends page
 #     c.save()
 
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Frame, PageTemplate
 from reportlab.lib.styles import getSampleStyleSheet
 styles = getSampleStyleSheet()
-mbta_logo = Image("images/mbtalogo.png")
-wsp_logo = Image("images/wsplogo.png")
-
+mbta_logo = "images/mbtalogo.png"
+wsp_logo = "images/wsplogo.png"
+styleN = styles["Normal"]
+styleT = styles["Title"]
 
 # formatting for the first page
 def first_page_format(canvas, doc):
     canvas.saveState()
+    canvas.drawImage(mbta_logo, .5*inch, HEIGHT-.25*inch, width=100, height=45)
+    canvas.drawImage(wsp_logo, WIDTH-2*inch, HEIGHT-.25*inch, width=100, height=45)
+    p = Paragraph("MBTA TUNNEL VENTILATION FACILITY AND SYSTEM ASSESSMENT", styleT)
+    w,h = p.wrap(inch*5, HEIGHT)
+    p.drawOn(canvas, WIDTH/2-w/2, HEIGHT-.25-h/2)
     canvas.setFont('Times-Roman',9)
     canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
     canvas.restoreState()
 
 
-# formatting for all other pages
-def later_page_format(canvas, doc):
-    canvas.saveState()
-    canvas.setFont('Times-Roman',9)
-    canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
-    canvas.restoreState()
+# # formatting for all other pages
+# def later_page_format(canvas, doc):
+#     canvas.saveState()
+#     canvas.setFont('Times-Roman',9)
+#     canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
+#     canvas.restoreState()
 
 
 def build_document():
-    doc = SimpleDocTemplate("test.pdf", pageSize=letter)
+    doc = SimpleDocTemplate("test.pdf", pageSize=letter, title="Test", author="WSP")  # start document template
+    # frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height, id='normal')  # frame for doc margins
+    # template = PageTemplate(id="test", frames=frame)
+    # doc.addPageTemplates([template])
+
     Story = []
-    style = styles["Normal"]
-    for i in range(100):
+
+    for i in range(10):
         bogustext = ("This is Paragraph number %s. " % i) * 20
-        p = Paragraph(bogustext, style)
+        p = Paragraph(bogustext, styleN)
         Story.append(p)
         Story.append(Image("images/10366.png", width=20, height=20))
         Story.append(Spacer(1,0.2*inch))
-    doc.build(Story, onFirstPage=first_page_format, onLaterPages=later_page_format)
+    doc.build(Story, onFirstPage=first_page_format, onLaterPages=first_page_format)
 
 
 if __name__ == '__main__':
