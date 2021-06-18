9a2c4fe74e8a1edd54a7a82b0c4c54188e619a0e
Tim <timothy.mcc@comcast.net>
2021-06-18 10:52:35 -0400

    linting

--- document.py
+++ document.py
@@ -16,6 +16,7 @@ styleT = ParagraphStyle(
     fontSize=14
 )
 
+
 class LocationInfo:
     def __init__(self, rail, location, insp_date):
         self.rail = rail
@@ -40,18 +41,19 @@ class Equipment:
 
 def create_equipment_table(equip):
     image = Image(equip.image_path)
-    image._restrictSize(2*inch, 2.5*inch)
+    image._restrictSize(2 * inch, 2.5 * inch)
     descr_p = Paragraph(equip.descr)
-    descr_p.wrap(4.75*inch, HEIGHT) # HEIGHT?
+    descr_p.wrap(4.75 * inch, HEIGHT)  # HEIGHT?
     sol_text_p = Paragraph(equip.sol_text)
     sol_text_p.wrap(4.75 * inch, HEIGHT)  # HEIGHT?
-    data =[["  " + equip.id, Paragraph('<b>Room:</b>'), equip.room, Paragraph('<b>Equipment ID:</b>'), equip.equipment_id,
-            Paragraph('<b>CS:</b> %d' % equip.cs)],
-           [equip.title, "", "", "", image],
-           [descr_p],
-           [equip.sol_title],
-           [sol_text_p],
-           ]
+    data = [
+        ["  " + equip.id, Paragraph('<b>Room:</b>'), equip.room, Paragraph('<b>Equipment ID:</b>'), equip.equipment_id,
+         Paragraph('<b>CS:</b> %d' % equip.cs)],
+        [equip.title, "", "", "", image],
+        [descr_p],
+        [equip.sol_title],
+        [sol_text_p],
+        ]
     t = Table(data, style=[('ALIGN', (0, 0), (-1, 0), 'CENTER'),  # align top row centered
                            ('ALIGN', (0, 1), (-1, 1), 'CENTER'),  # align title and photo
                            ('ALIGN', (0, 3), (0, 3), 'CENTER'),  # align solution title centered
@@ -69,15 +71,17 @@ def create_equipment_table(equip):
                            ('BACKGROUND', (0, 1), (3, 1), colors.pink),
                            ('BACKGROUND', (0, 3), (3, 3), colors.lightgreen),
                            ],
-              colWidths=[.25*inch, .75*inch, 2.5*inch, 1.25*inch, 1.9*inch, .6*inch],
-              rowHeights=[.25*inch, .25*inch, 1.25*inch, .25*inch, 1*inch])
+              colWidths=[.25 * inch, .75 * inch, 2.5 * inch, 1.25 * inch, 1.9 * inch, .6 * inch],
+              rowHeights=[.25 * inch, .25 * inch, 1.25 * inch, .25 * inch, 1 * inch])
     return t
 
 
 def create_report_table(loc):
-    data =[[Paragraph('<b>RAIL LINE:</b> %s' % loc.rail), Paragraph('<b>INSPECTION DATE:</b> %s' % loc.insp_date)], # change date data type?
-           [Paragraph('<b>LOCATION:</b> %s' % loc.location)],
-           ]
+    data = [
+        [Paragraph('<b>RAIL LINE:</b> %s' % loc.rail), Paragraph('<b>INSPECTION DATE:</b> %s' % loc.insp_date)],
+        # change date data type?
+        [Paragraph('<b>LOCATION:</b> %s' % loc.location)],
+            ]
     t = Table(data)
     return t
 
@@ -87,18 +91,18 @@ def first_page_format(canvas, doc):
     canvas.saveState()
 
     # setup header
-    canvas.drawImage(mbta_logo, .45*inch, HEIGHT-.05*inch, width=100, height=19)
-    canvas.drawImage(wsp_logo, WIDTH-1.65*inch, HEIGHT-.2*inch, width=70, height=33)
+    canvas.drawImage(mbta_logo, .45 * inch, HEIGHT - .05 * inch, width=100, height=19)
+    canvas.drawImage(wsp_logo, WIDTH - 1.65 * inch, HEIGHT - .2 * inch, width=70, height=33)
     p = Paragraph("MBTA TUNNEL VENTILATION FACILITY AND SYSTEM ASSESSMENT", styleT)
-    w, h = p.wrap(inch*4, HEIGHT)
-    p.drawOn(canvas, (WIDTH-16)/2-w/2, HEIGHT-.25-h/2)
+    w, h = p.wrap(inch * 4, HEIGHT)
+    p.drawOn(canvas, (WIDTH - 16) / 2 - w / 2, HEIGHT - .25 - h / 2)
 
     # setup footer
-    canvas.setFont('Times-Roman',9)
-    canvas.drawString(.75*inch, .75*inch, "%s" % date.today().strftime("%m/%d/%Y"))
-    canvas.drawCentredString((WIDTH-16)/2, .75*inch, "Page %d" % doc.page)
-    canvas.drawRightString(WIDTH-inch, .90*inch, "Contract No. Z94PS10")
-    canvas.drawRightString(WIDTH-inch, .75*inch, "Task Order: 03")
+    canvas.setFont('Times-Roman', 9)
+    canvas.drawString(.75 * inch, .75 * inch, "%s" % date.today().strftime("%m/%d/%Y"))
+    canvas.drawCentredString((WIDTH - 16) / 2, .75 * inch, "Page %d" % doc.page)
+    canvas.drawRightString(WIDTH - inch, .90 * inch, "Contract No. Z94PS10")
+    canvas.drawRightString(WIDTH - inch, .75 * inch, "Task Order: 03")
     canvas.restoreState()
 
 
@@ -116,17 +120,17 @@ def build_document():
     Story = []
 
     t = create_report_table(LocationInfo("RED LINE", "Vent Shaft R-13 (Cabot Yard)", "5/8/2021"))
-    Story.append(t) # add location information
+    Story.append(t)  # add location information
     Story.append(Spacer(1, 0.2 * inch))
 
-    for i in range(10): # for row in spreadsheet
+    for i in range(10):  # for row in spreadsheet
         e = Equipment("E", 1, "EF-2 EXHAUST PLENUM", "PUSHBUTTON", 3, "CORROSION",
                       "PUSHBUTTON IS CORRODED. DEVICE IS STILL OPERATIONAL, BUT SHOULD BE MONITORED.",
                       "MONITOR", "MONITOR DEVICE OVER COMING YEARS", "images/pushbutton.png")  # get from sheet
         t = create_equipment_table(e)
 
         Story.append(KeepTogether(t))
-        Story.append(Spacer(1,0.1*inch))
+        Story.append(Spacer(1, 0.1 * inch))
 
     doc.build(Story, onFirstPage=first_page_format, onLaterPages=first_page_format)
 
