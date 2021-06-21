2c5a09f911e7424c38f9a5c00d7de0046da30010
Tim McCarthy <temccarthy@wpi.edu>
2021-06-21 09:53:40 -0400

    created ui

--- gui.py
+++ gui.py
@@ -1,7 +1,25 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
+from PyQt5 import uic
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi("WOIDS.ui", self)
+
+        self.browse_button = self.findChild(QPushButton, "browse_button")
+        self.folder_path_line = self.findChild(QLineEdit, "folder_path")
+        self.check_button = self.findChild(QPushButton, "check_button")
+        self.generate_button = self.findChild(QPushButton, "generate_button")
+        self.template_button = self.findChild(QPushButton, "template_button")
+
+        self.show()
+
+    
+
 
 if __name__ == "__main__":
-    app = QApplication([])
-    label = QLabel("test")
-    label.show()
+    app = QApplication(sys.argv)
+    window = MainWindow()
     app.exec()
