d14b0a6409a979879490c73d55bf1af2bf9de90b
Tim McCarthy <temccarthy@wpi.edu>
2021-06-21 10:04:44 -0400

    connected buttons to action stubs

--- gui.py
+++ gui.py
@@ -8,15 +8,33 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi("WOIDS.ui", self)
 
+        # connect widgets to python
         self.browse_button = self.findChild(QPushButton, "browse_button")
         self.folder_path_line = self.findChild(QLineEdit, "folder_path")
         self.check_button = self.findChild(QPushButton, "check_button")
         self.generate_button = self.findChild(QPushButton, "generate_button")
         self.template_button = self.findChild(QPushButton, "template_button")
 
+        # connect button presses to actions
+        self.browse_button.clicked.connect(self.browse_folders)
+        self.check_button.clicked.connect(self.check_folders)
+        self.generate_button.clicked.connect(self.generate_report)
+        self.template_button.clicked.connect(self.generate_template)
+
         self.show()
 
-    
+    def browse_folders(self):
+        print(self.folder_path_line.text())
+
+    def check_folders(self):
+        self.folder_path_line.setText("test")
+        pass
+
+    def generate_report(self):
+        pass
+
+    def generate_template(self):
+        pass
 
 
 if __name__ == "__main__":
