353cba4008be197de4f1e34cc143e4520c9010af
Tim <timothy.mcc@comcast.net>
2021-08-03 11:22:13 -0400

    finished first draft of new layout

--- document.py
+++ document.py
@@ -67,27 +67,25 @@ def create_equipment_table(equip):
          Paragraph('<b>Component:</b>'), equip.component],
         [Paragraph('<b>Deficiency/Notes: </b>'), "", "", "", image],
         [notes],
-        [],
-        [],
         ]
     t = Table(data, style=[('ALIGN', (0, 0), (-1, 0), 'CENTER'),  # align top row centered
                            ('ALIGN', (3, 1), (-1, 1), 'CENTER'),  # align title and photo
                            # ('ALIGN', (0, 3), (0, 3), 'CENTER'),  # align solution title centered
                            ('VALIGN', (0, 2), (0, 2), 'TOP'),
-                           ('VALIGN', (0, 4), (0, 4), 'TOP'),
-                           ('VALIGN', (4, 1), (-1, -1), 'CENTER'),
+                           # ('VALIGN', (0, 4), (0, 4), 'TOP'),
+                           ('VALIGN', (-1, 1), (-1, -1), 'CENTER'),
                            ('BOX', (0, 0), (-1, -1), 1, colors.black),
                            ('BOX', (0, 0), (-1, 0), 1, colors.black),
                            ('GRID', (0, 1), (3, -1), 1, colors.black),
                            ('SPAN', (0, 1), (3, 1)),  # span title
                            ('SPAN', (0, 2), (3, 2)),  # span descr
-                           ('SPAN', (0, 3), (3, 3)),  # span sol title
-                           ('SPAN', (0, 4), (3, 4)),  # span sol descr
-                           ('SPAN', (-2, 1), (-1, -1)),  # span picture box
+                           # ('SPAN', (0, 3), (3, 3)),  # span sol title
+                           # ('SPAN', (0, 4), (3, 4)),  # span sol descr
+                           ('SPAN', (-1, 1), (-1, -1)),  # span picture box
                            # ('BACKGROUND', (-1, 0), (-1, 0), cs_colors[equip.cs-1])
                            ],
-              colWidths=[.4 * inch, .75 * inch, 2.3 * inch, 1.25 * inch, 1.9 * inch, .6 * inch],
-              rowHeights=[.25 * inch, .25 * inch, 1.25 * inch, .25 * inch, 1 * inch])
+              colWidths=[.4 * inch, 1.25 * inch, (2.3-.3) * inch, 1.05 * inch, 2.5 * inch],
+              rowHeights=[.25 * inch, .25 * inch, 2.5 * inch])
     return t
 
 
@@ -107,7 +105,7 @@ def first_page_format(canvas, doc):
     canvas.saveState()
 
     # setup header
-    canvas.drawImage(seattle_logo, .45 * inch, HEIGHT - .05 * inch, width=100, height=19)
+    canvas.drawImage(seattle_logo, .45 * inch, HEIGHT - .05 * inch, width=100, height=15)
     canvas.drawImage(wsp_logo, WIDTH - 1.65 * inch, HEIGHT - .2 * inch, width=70, height=33)
     p = Paragraph("DSTT NTIS INSPECTION REPORT", styleT)
     w, h = p.wrap(inch * 4, HEIGHT)
@@ -115,10 +113,10 @@ def first_page_format(canvas, doc):
 
     # setup footer
     canvas.setFont('Times-Roman', 9)
-    canvas.drawString(.75 * inch, .75 * inch, "%s" % date.today().strftime("%m/%d/%Y"))
+    canvas.drawString(.75 * inch, .75 * inch, "%s" % date.today().strftime("%B %d, %Y"))
     canvas.drawCentredString((WIDTH - 16) / 2, .75 * inch, "Page %d" % doc.page)
-    canvas.drawRightString(WIDTH - inch, .90 * inch, "Contract No. Z94PS10")
-    canvas.drawRightString(WIDTH - inch, .75 * inch, "Task Order: 03")
+    canvas.drawRightString(WIDTH - inch, .90 * inch, "Contract No. 160415P-01")
+    canvas.drawRightString(WIDTH - inch, .75 * inch, "Task Order: 17.04.02")
     canvas.restoreState()
 
 
